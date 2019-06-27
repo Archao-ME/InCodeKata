dd2142fe03ccc4af9952c2990df45935ebf3334f
jichao chen <jichaochen@jichaodeMacBook-Pro.local>
2019-06-27 22:30:30 +0800

    5分钟完成；

--- FizzBuzz/FizzBuzz.spec.js
+++ FizzBuzz/FizzBuzz.spec.js
@@ -1,32 +1,46 @@
-function fizzBuzz (param) {
-    if (param % 3 === 0 && param % 5 === 0) {
+function getFizzBuzz(number) {
+    if (number % 3 === 0 && number % 5 === 0) {
         return 'FizzBuzz'
     }
-    if (param % 3 === 0) {
+    if (number.toString().indexOf('5') >= 0 && number.toString().indexOf('3') >= 0) {
+        return 'FizzBuzz'
+    }
+    if (number % 3 === 0) {
         return 'Fizz'
     }
-    if (param % 5 === 0) {
+    if (number % 5 === 0) {
         return 'Buzz'
     }
-    return param
+    if(number.toString().indexOf('3') >= 0) {
+        return 'Fizz'
+    }
+
+    if (number.toString().indexOf('5') >= 0) {
+        return 'Buzz'
+    }
 }
-
-test('should be buzz if it can be divisible by 3 ', () => {
-    expect(fizzBuzz(3)).toEqual('Fizz');
-    expect(fizzBuzz(6)).toEqual('Fizz');
+// TODO: 用Each遍历测试
+test('should return Fizz if input 3', () => {
+    expect(getFizzBuzz(3)).toEqual('Fizz')
+})
+test('should return Buzz if input 5', () => {
+    expect(getFizzBuzz(5)).toEqual('Buzz')
 })
 
-test('should be buzz if it can be divisible by 5 ', () => {
-    expect(fizzBuzz(5)).toEqual('Buzz')
-    expect(fizzBuzz(20)).toEqual('Buzz')
+test('should return FizzBuzz if input 15', () => {
+    expect(getFizzBuzz(15)).toEqual('FizzBuzz')
 })
 
-it('should be FizzBuzz when it is divisible by 3 and 5', function () {
-    expect(fizzBuzz(15)).toEqual('FizzBuzz')
+test('should return Fizz if includes "3"', () => {
+    expect(getFizzBuzz(13)).toEqual('Fizz')
 })
 
-test('should return itself when it is not divisible by 3 or 5', () => {
-    expect(fizzBuzz(2)).toEqual(2)
+test('should return Buzz if includes "5"', () => {
+    expect(getFizzBuzz(52)).toEqual('Buzz')
+})
+
+test('should return Buzz if includes "5" and "3"', () => {
+    expect(getFizzBuzz(53)).toEqual('FizzBuzz')
 })
 
 
