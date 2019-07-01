248c74fb836114673fc121378b9110b68b5ee914
jichao chen <jichaochen@jichaodeMacBook-Pro.local>
2019-07-01 22:52:20 +0800

    3:20 FizzBuzz

--- FizzBuzz/FizzBuzz.spec.js
+++ FizzBuzz/FizzBuzz.spec.js
@@ -1,50 +1,34 @@
-function getFizzBuzz(number) {
-    if (number % 3 === 0 && number % 5 === 0) {
+function fizzBuzz(num) {
+    if (num% 3 === 0 && num % 5 === 0) {
         return 'FizzBuzz'
     }
-    if (number.toString().indexOf('5') >= 0 && number.toString().indexOf('3') >= 0) {
+    if (num.toString().includes('3') && num.toString().includes('5')) {
         return 'FizzBuzz'
     }
-    if (number % 3 === 0) {
+    if (num % 3 === 0) {
         return 'Fizz'
     }
-    if (number % 5 === 0) {
+    if (num % 5 === 0) {
         return 'Buzz'
-    }
-    if(number.toString().indexOf('3') >= 0) {
-        return 'Fizz'
     }
 
-    if (number.toString().indexOf('5') >= 0) {
+    if (num.toString().includes('3')) {
+        return 'Fizz'
+    }
+    if (num.toString().includes('5')) {
         return 'Buzz'
     }
-    return number
+    return num;
 }
-// TODO: 用Each遍历测试
-test('should return itself', () => {
-    expect(getFizzBuzz(4)).toEqual(4)
-})
-test('should return Fizz if input 3', () => {
-    expect(getFizzBuzz(3)).toEqual('Fizz')
-})
-test('should return Buzz if input 5', () => {
-    expect(getFizzBuzz(5)).toEqual('Buzz')
-})
 
-test('should return FizzBuzz if input 15', () => {
-    expect(getFizzBuzz(15)).toEqual('FizzBuzz')
+test.each([
+    [1, 1],
+    [3, 'Fizz'],
+    [5, 'Buzz'],
+    [15, 'FizzBuzz'],
+    [31, 'Fizz'],
+    [52, 'Buzz'],
+    [53, 'FizzBuzz']
+])('Input %s ouput %s', (a, b) => {
+    expect(fizzBuzz(a)).toEqual(b)
 })
-
-test('should return Fizz if includes "3"', () => {
-    expect(getFizzBuzz(13)).toEqual('Fizz')
-})
-
-test('should return Buzz if includes "5"', () => {
-    expect(getFizzBuzz(52)).toEqual('Buzz')
-})
-
-test('should return Buzz if includes "5" and "3"', () => {
-    expect(getFizzBuzz(53)).toEqual('FizzBuzz')
-})
-
-
